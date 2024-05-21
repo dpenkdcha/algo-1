c677d7626bd912afed7cd8cf6b9049b5e5ee24a7
dpenkdcha <dpenkdcha@gmail.com>
2024-05-21 13:48:32 +0530

    week1

---
+++ RandomWord.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class RandomWord {
+  public static void main(String[] args) {
+    String champion = null;
+    int i = 0;
+
+    while (StdIn.isEmpty()) {
+      String word = StdIn.readString();
+      i++;
+
+      if (Math.random() < 1.0 / i) {
+        champion = word;
+      }
+    }
+
+    if (champion != null) {
+      StdOut.println(champion);
+    }
+  }
+}
